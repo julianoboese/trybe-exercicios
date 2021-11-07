3cebc087556339060da8596ac19c10e64031b58a
Juliano Boese <juliano.boese@gmail.com>
2021-11-07 19:11:11 -0300

    Exercicio 4.1 - Programas 6 e 7

--- fundamentos/bloco-04-indroducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-indroducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -85,3 +85,59 @@ if (angle1 <= 0 || angle2 <= 0 || angle3 <= 0) {
 }
 
 console.log(result);
+
+// Programa 6
+const chessPiece = "Queen";
+
+switch (chessPiece.toLocaleLowerCase()) {
+  case "king":
+    result =
+      "Kings move one square in any direction, so long as that square is not attacked by an enemy piece. Additionally, kings are able to make a special move, known as castling.";
+    break;
+  case "queen":
+    result =
+      "Queens move diagonally, horizontally, or vertically any number of squares. They are unable to jump over pieces.";
+    break;
+  case "rook":
+    result =
+      "Rooks move horizontally or vertically any number of squares. They are unable to jump over pieces. Rooks move when the king castles.";
+    break;
+  case "knight":
+    result =
+      "Knights move in an ‘L’ shape’: two squares in a horizontal or vertical direction, then move one square horizontally or vertically. They are the only piece able to jump over other pieces.";
+    break;
+  case "bishop":
+    result =
+      "Bishops move diagonally any number of squares. They are unable to jump over pieces.";
+    break;
+  case "pawn":
+    result =
+      "Pawns move vertically forward one square, with the option to move two squares if they have not yet moved. Pawns are the only piece to capture different to how they move. The pawns capture one square diagonally in a forward direction.      Pawns are unable to move backward on captures or moves. Upon reaching the other side of the board a pawn promotes into any other piece, except for a king. Additionally, pawns can make a special move named En Passant.";
+    break;
+  default:
+    result = "Choose a Chess Piece";
+    break;
+}
+
+console.log(result);
+
+// Programa 7
+const grade = 80;
+
+if (grade > 100 || grade < 0) {
+  result = "The grade must be between 0 and 100";
+} else if (grade >= 90) {
+  result = "A";
+} else if (grade >= 80) {
+  result = "B";
+} else if (grade >= 70) {
+  result = "C";
+} else if (grade >= 60) {
+  result = "D";
+} else if (grade >= 50) {
+  result = "E";
+} else {
+  result = "F";
+}
+
+console.log(result);
