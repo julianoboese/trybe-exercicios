95ea6720a28b3c00f443807f4584a8e660373784
Juliano Boese <juliano.boese@gmail.com>
2021-11-07 18:24:07 -0300

    Exercicio 4.1 - Programas 4 e 5

--- fundamentos/bloco-04-indroducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-indroducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -39,3 +39,49 @@ if (e > f) {
 }
 
 console.log(result);
+
+// Programa 4
+// Utilizando if
+const h = 0;
+
+if (h > 0) {
+  result = "positive";
+} else if (h < 0) {
+  result = "negative";
+} else {
+  result = "zero";
+}
+
+console.log(result);
+
+// Utilizando switch
+switch (h / Math.abs(h)) {
+  case 1:
+    result = "positive";
+    break;
+  case -1:
+    result = "negative";
+    break;
+  default:
+    result = "zero";
+    break;
+}
+
+console.log(result);
+
+// Programa 5
+const angle1 = 60;
+const angle2 = 90;
+const angle3 = 30;
+
+if (angle1 <= 0 || angle2 <= 0 || angle3 <= 0) {
+  result = "Error: all the angles must be positive";
+} else if (angle1 >= 180 || angle2 >= 180 || angle3 >= 180) {
+  result = "Error: no angle can be greater than 180";
+} else if (angle1 + angle2 + angle3 === 180) {
+  result = true;
+} else {
+  result = false;
+}
+
+console.log(result);
