afe21c88b5cfe5754ff42b77f5a538f928d48332
Juliano Boese <juliano.boese@gmail.com>
2021-11-08 13:19:32 -0300

    Exercicio 4.2 - Exercicios 3 e 4

--- fundamentos/bloco-04-indroducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-indroducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -14,3 +14,30 @@ for (i = 0; i < numbers.length; i+=1) {
 }
 
 console.log(sum);
+
+// Exercício 3
+sum = 0;
+
+for (i = 0; i < numbers.length; i+=1) {
+  sum+=numbers[i];
+}
+
+let average = sum / numbers.length;
+
+console.log(average);
+
+// Exercício 4
+sum = 0;
+
+for (i = 0; i < numbers.length; i+=1) {
+  sum+=numbers[i];
+}
+
+average = sum / numbers.length;
+
+if (average > 20) {
+  console.log('valor maior que 20');
+} else {
+  console.log('valor menor ou igual a 20');
+}
+
