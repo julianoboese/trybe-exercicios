9b4389682e2bb3ff3304cfe92525306e76958b99
Juliano Boese <juliano.boese@gmail.com>
2021-11-08 13:36:47 -0300

    Exercicio 4.2 - Exercicios 5 e 6

--- fundamentos/bloco-04-indroducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-indroducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -1,16 +1,16 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let i
+let i;
 
 // Exercício 1
-for (i = 0; i < numbers.length; i+=1) {
+for (i = 0; i < numbers.length; i += 1) {
   console.log(numbers[i]);
 }
 
 // Exercício 2
 let sum = 0;
 
-for (i = 0; i < numbers.length; i+=1) {
-  sum+=numbers[i];
+for (i = 0; i < numbers.length; i += 1) {
+  sum += numbers[i];
 }
 
 console.log(sum);
@@ -18,8 +18,8 @@ console.log(sum);
 // Exercício 3
 sum = 0;
 
-for (i = 0; i < numbers.length; i+=1) {
-  sum+=numbers[i];
+for (i = 0; i < numbers.length; i += 1) {
+  sum += numbers[i];
 }
 
 let average = sum / numbers.length;
@@ -29,15 +29,41 @@ console.log(average);
 // Exercício 4
 sum = 0;
 
-for (i = 0; i < numbers.length; i+=1) {
-  sum+=numbers[i];
+for (i = 0; i < numbers.length; i += 1) {
+  sum += numbers[i];
 }
 
 average = sum / numbers.length;
 
 if (average > 20) {
-  console.log('valor maior que 20');
+  console.log("valor maior que 20");
 } else {
-  console.log('valor menor ou igual a 20');
+  console.log("valor menor ou igual a 20");
+}
+
+// Exercício 5
+let higherNumber = numbers[0];
+
+for (i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] > higherNumber) {
+    higherNumber = numbers[i];
+  }
+}
+
+console.log(higherNumber);
+
+// Exercício 6
+let oddNumbers = 0;
+
+for (i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] % 2 !== 0) {
+    oddNumbers += 1;
+  }
+}
+
+if (oddNumbers === 0) {
+  console.log("nenhum valor ímpar encontrado")
+} else {
+  console.log(oddNumbers);
 }
 
