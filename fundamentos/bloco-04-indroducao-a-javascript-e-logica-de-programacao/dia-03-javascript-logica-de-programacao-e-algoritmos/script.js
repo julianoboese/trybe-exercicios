dcee08ffb95162f18f06b642be433e1c43d228c0
Juliano Boese <juliano.boese@gmail.com>
2021-11-09 15:05:43 -0300

    Exercicio 4.3 - Exercicios 1, 2 e 3

---
+++ fundamentos/bloco-04-indroducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -0,0 +1,35 @@
+// Exercício 1
+let n = 5;
+let line = "";
+
+for (let i = 1; i <= n; i += 1) {
+  line += "*";
+}
+
+for (let i = 1; i <= n; i += 1) {
+  console.log(line);
+}
+
+// Exercício 2
+line = "";
+
+for (let i = 1; i <= n; i += 1) {
+  line += "*";
+  console.log(line);
+}
+
+// Exercício 3
+line = "";
+
+for (let i = 1; i <= n; i += 1) {
+  for (let j = 1; j <= n; j += 1) {
+    if (j <= n - i) {
+      line += " ";
+    } else {
+      line += "*";
+    }
+  }
+  console.log(line);
+    line = ""
+}
+
