878ba1afcddaab64c5ce78d1dfc578737df6e9e2
Juliano Boese <juliano.boese@gmail.com>
2021-11-18 14:15:11 -0300

    Exercício 5.2 - Exercícios 5 a 9

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/script.js
@@ -8,8 +8,36 @@ document.body.appendChild(main);
 
 const section = document.createElement('section');
 section.className = 'center-content';
-document.querySelector('main').appendChild(section);
+main.appendChild(section);
 
 const p = document.createElement('p');
 p.innerText = 'Exercícios DOM';
-document.querySelector('.center-content').appendChild(p);
+section.appendChild(p);
+
+const sectionLeft = document.createElement('section');
+sectionLeft.className = 'left-content';
+main.appendChild(sectionLeft);
+
+const sectionRight = document.createElement('section');
+sectionRight.className = 'right-content';
+main.appendChild(sectionRight);
+
+const imagem = document.createElement('img');
+imagem.src = 'https://picsum.photos/200';
+imagem.className = 'small-image';
+sectionLeft.appendChild(imagem);
+
+const list = document.createElement('ul');
+sectionRight.appendChild(list);
+const values = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'];
+for (let i = 0; i < values.length; i += 1) {
+  const listItem = document.createElement('li');
+  listItem.innerText = values[i];
+  list.appendChild(listItem);
+}
+
+for (let i = 1; i <= 3; i += 1) {
+  const h3 = document.createElement('h3');
+  h3.innerText = `Tag ${i}`;
+  main.appendChild(h3);
+}
