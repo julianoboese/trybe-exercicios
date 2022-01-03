05c11bc45fc57531ce989cca842dfa9adf780cd5
Juliano Boese <juliano.boese@gmail.com>
2022-01-03 19:34:58 -0300

    Exercício 8.4 - Exercício 6

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/script.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/script.js
@@ -109,3 +109,16 @@ const containsA = () => names.reduce((acc, name) => (
 ), 0);
 
 console.log(containsA());
+
+// Exercício 6
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+const studentAverage = () => students.map((student, index) => (
+  {
+    name: student,
+    average: grades[index].reduce((acc, grade) => acc + grade) / grades[index].length,
+  }
+));
+
+console.log(studentAverage());
