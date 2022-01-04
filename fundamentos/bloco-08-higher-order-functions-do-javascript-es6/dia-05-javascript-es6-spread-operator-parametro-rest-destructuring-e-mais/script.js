fcc14c859b949a5d29c39ffd88c7cd55516b87d8
Juliano Boese <juliano.boese@gmail.com>
2022-01-04 11:24:37 -0300

    Exercício 8.5 - Exercícios 1 a 3

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/script.js
@@ -0,0 +1,36 @@
+// Exercício 1
+const rectangleArea = (width, height) => width * height;
+
+const rectangle1 = [1, 2];
+const rectangle2 = [3, 5];
+const rectangle3 = [6, 9];
+const rectangles = [rectangle1, rectangle2, rectangle3];
+
+rectangles.forEach((rectangle) => {
+  console.log(rectangleArea(...rectangle));
+});
+
+// Exercício 2
+const sum = (...args) => args.reduce((acc, arg) => acc + arg, 0);
+
+console.log(sum(1, 2, 3, 4, 5));
+
+// Exercício 3
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+console.log(personLikes(alex));
+console.log(personLikes(gunnar));
