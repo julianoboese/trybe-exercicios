bea06398d7b3a62542fb73381a5c58cc288ecdd6
Juliano Boese <juliano.boese@gmail.com>
2022-01-11 21:55:50 -0300

    Exercício 9.1 - Exercícios 5 e 6

--- fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/script.js
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/script.js
@@ -10,9 +10,9 @@ const getPlanet = () => {
   console.log('Returned planet: ', mars);
 };
 
-// setTimeout(() => getPlanet(), 4000); // imprime Marte depois de 4 segundos
+// setTimeout(() => getPlanet(), 4000);
 
-// Exercício 4
+// Exercícios 4, 5 e 6
 const messageDelay = () => Math.floor(Math.random() * 5000);
 
 const getMarsTemperature = () => {
@@ -20,8 +20,17 @@ const getMarsTemperature = () => {
   return Math.floor(Math.random() * maxTemperature);
 };
 
-const sendMarsTemperature = () => setTimeout(() => {
-  console.log(`Mars temperature is: ${getMarsTemperature()} degrees Celsius`);
-}, messageDelay());
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
 
-sendMarsTemperature(); // imprime "Mars temperature is: 20 degrees Celsius", por exemplo
+const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
+
+const sendMarsTemperature = (message, error) => setTimeout(() => (
+  Math.random() <= 0.6 ? message(getMarsTemperature()) : error('Robot is busy')
+), messageDelay());
+
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+sendMarsTemperature(greet, handleError);
