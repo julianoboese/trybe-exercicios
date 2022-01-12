ba00971fb6984de6a1d2d9baa6fcfa67abc64597
Juliano Boese <juliano.boese@gmail.com>
2022-01-11 21:38:40 -0300

    Exercício 9.1 - Exercícios 3 e 4

---
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/script.js
@@ -0,0 +1,27 @@
+// Exercício 3
+const getPlanet = () => {
+  const mars = {
+    name: 'Mars',
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: 'kilometers',
+    },
+  };
+  console.log('Returned planet: ', mars);
+};
+
+// setTimeout(() => getPlanet(), 4000); // imprime Marte depois de 4 segundos
+
+// Exercício 4
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const sendMarsTemperature = () => setTimeout(() => {
+  console.log(`Mars temperature is: ${getMarsTemperature()} degrees Celsius`);
+}, messageDelay());
+
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degrees Celsius", por exemplo
