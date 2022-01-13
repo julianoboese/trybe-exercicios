8f02df7332cc1e7eb918b13eeb68a319df1e77d5
Juliano Boese <juliano.boese@gmail.com>
2022-01-12 23:42:13 -0300

    Exercício 9.2 - Exercício Bônus

--- fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assíncrono-fetch-api-e-async-await/api.js
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assíncrono-fetch-api-e-async-await/api.js
@@ -1,23 +1,26 @@
-const fetchCrypto = () => {
+// Exercício 2
+const fetchCrypto = async () => {
   const list = document.getElementById('cryptos');
 
-  const requestOptions = {
-    method: 'GET',
-    redirect: 'follow',
-  };
+  const currencyResponse = await fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/brl.json');
+  const currencyData = await currencyResponse.json();
+  const conversionFactor = currencyData.brl;
+  console.log(conversionFactor);
 
-  fetch('https://api.coincap.io/v2/assets', requestOptions)
+  fetch('https://api.coincap.io/v2/assets')
     .then((response) => response.json())
     .then(({ data }) => {
       data
         .filter((_, index) => index <= 10)
         .forEach(({ name, symbol, priceUsd }) => {
           const listItem = document.createElement('li');
-          listItem.innerHTML = `${name} (${symbol}): $ ${parseFloat(priceUsd).toFixed(2)}`;
+          listItem.innerHTML = `${name} (${symbol}): R$ ${parseFloat(priceUsd * conversionFactor).toFixed(2)}`;
           list.appendChild(listItem);
         });
     })
     .catch((error) => console.log('error', error));
 };
 
-window.onload = fetchCrypto();
+window.onload = () => {
+  fetchCrypto();
+};
