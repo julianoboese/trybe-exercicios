a17c37e77e6cb163203e88588e779629cc3ba86d
Juliano Boese <juliano.boese@gmail.com>
2022-01-21 15:16:22 -0300

    Exercício 10.1 - Exercício 3

---
+++ front-end/bloco-10-introdução-a-react/dia-01-hello-world-no-react/second-app/src/Content.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const conteudos = [
+  {
+    conteudo: 'High Order Functions',
+    bloco: 8,
+    status: 'Aprendido'
+  },
+  {
+    conteudo: 'Composicao de Componentes',
+    bloco: 11,
+    status: 'Aprendendo',
+  },
+  {
+    conteudo: 'Composicao de Estados',
+    bloco: 12,
+    status: 'Aprenderei'
+  },
+  {
+    conteudo: 'Redux',
+    bloco: 16,
+    status: 'Aprenderei'
+  },
+];
+
+class Content extends React.Component {
+  render() {
+    return(conteudos.map((item) => (
+      <div key={item.conteudo}>
+        <h2>O conteúdo é: {item.conteudo}</h2>
+        <p>Status: {item.status}</p>
+        <p>Bloco: {item.bloco}</p>
+      </div>
+      )
+      )
+    ) 
+  }
+}
+
+export default Content
