2791ca477882e14a716b4b0501080a0b7e387457
Juliano Boese <juliano.boese@gmail.com>
2022-02-01 22:10:23 -0300

    Adiciona combobox

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/my-form-2.0/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/my-form-2.0/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import Input from './components/Input';
 import './App.css';
 
 class App extends React.Component {
@@ -7,18 +10,57 @@ class App extends React.Component {
     email: '',
     cpf: '',
     address: '',
+    city: '',
+  };
+
+  handleChange = ({ target }) => {
+    switch (target.name) {
+      case 'address':
+        this.setState({ address: target.value.replace(/[^\w\s]/gi, '') });
+        break;
+      default:
+        this.setState({ [target.name]: target.value });
+    }
+  };
+
+  handleBlur = ({ target }) => {
+    if (Number.isInteger(parseInt(target.value.charAt(0), 10))) {
+      this.setState({ [target.name]: '' });
+    }
   };
 
   render() {
+    const {
+      name, email, cpf, address, city,
+    } = this.state;
+
+    const options = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS',
+      'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
+
     return (
-      <div className="App">
+      <div className="form-container">
         <form>
           <fieldset>
-            <input type="text" maxLength="40" style={{ textTransform: 'uppercase' }} required />
-            <input type="text" maxLength="50" required />
-            <input type="text" maxLength="11" required />
-            <input type="text" maxLength="200" onChange required />
-            <input type="text" maxLength="28" onBlur required />
+            <Input labelText="Nome" type="text" name="name" value={name} maxLength="40" onInputChange={this.handleChange} />
+            <Input labelText="E-mail" type="text" name="email" value={email} maxLength="50" onInputChange={this.handleChange} />
+            <Input labelText="CPF" type="text" name="cpf" value={cpf} maxLength="11" onInputChange={this.handleChange} />
+            <Input labelText="Endereço" type="text" name="address" value={address} maxLength="200" onInputChange={this.handleChange} />
+            <Input
+              labelText="Cidade"
+              type="text"
+              name="city"
+              value={city}
+              maxLength="200"
+              onInputChange={this.handleChange}
+              onInputBlur={this.handleBlur}
+            />
+            <div className="combobox">
+              <Autocomplete
+                options={options}
+                style={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="Estado" variant="outlined" />}
+              />
+            </div>
           </fieldset>
         </form>
       </div>
--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/my-form-2.0/src/components/Input.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/my-form-2.0/src/components/Input.js
@@ -3,16 +3,35 @@ import PropTypes from 'prop-types';
 
 function Input(props) {
   const {
-    labelText, type, name, value, maxLength,
+    labelText, type, name, value, maxLength, onInputChange, onInputBlur,
   } = props;
   return (
-    <div>
+    <div className="input-field">
       <label htmlFor={name}>{labelText}</label>
-      <input type={type} name={name} value={value} maxLength={maxLength} />
+      <input
+        type={type}
+        name={name}
+        value={value}
+        maxLength={maxLength}
+        onChange={onInputChange}
+        onBlur={onInputBlur}
+      />
     </div>
   );
 }
 
-Input.propTypes = {};
+Input.defaultProps = {
+  onInputBlur: () => {},
+};
+
+Input.propTypes = {
+  labelText: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  maxLength: PropTypes.string.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onInputBlur: PropTypes.func,
+};
 
 export default Input;
