eca9c056eb31948e740701ad76f3569553be1fa5
Juliano Boese <juliano.boese@gmail.com>
2022-02-02 00:40:30 -0300

    Finaliza inputs

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/my-form-2.0/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/my-form-2.0/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import Input from './components/Input';
+import InputCheck from './components/InputCheck';
 import './App.css';
 
 class App extends React.Component {
@@ -11,6 +12,12 @@ class App extends React.Component {
     cpf: '',
     address: '',
     city: '',
+    state: '',
+    type: '',
+    resume: '',
+    position: '',
+    description: '',
+    alertShown: false,
   };
 
   handleChange = ({ target }) => {
@@ -29,9 +36,17 @@ class App extends React.Component {
     }
   };
 
+  handleMouseEnter = () => {
+    const { alertShown } = this.state;
+    if (!alertShown) {
+      alert('Preencha com cuidado esta informação.');
+      this.setState({ alertShown: true });
+    }
+  };
+
   render() {
     const {
-      name, email, cpf, address, city,
+      name, email, cpf, address, city, state, type, resume, position, description,
     } = this.state;
 
     const options = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS',
@@ -40,7 +55,7 @@ class App extends React.Component {
     return (
       <div className="form-container">
         <form>
-          <fieldset>
+          <fieldset className="first-section">
             <Input labelText="Nome" type="text" name="name" value={name} maxLength="40" onInputChange={this.handleChange} />
             <Input labelText="E-mail" type="text" name="email" value={email} maxLength="50" onInputChange={this.handleChange} />
             <Input labelText="CPF" type="text" name="cpf" value={cpf} maxLength="11" onInputChange={this.handleChange} />
@@ -56,11 +71,53 @@ class App extends React.Component {
             />
             <div className="combobox">
               <Autocomplete
+                inputValue={state}
+                onInputChange={(_event, newInputValue) => {
+                  this.setState({ state: newInputValue });
+                }}
                 options={options}
-                style={{ width: 300 }}
                 renderInput={(params) => <TextField {...params} label="Estado" variant="outlined" />}
               />
             </div>
+            <p>Tipo</p>
+            <InputCheck labelText="Casa" type="radio" name="type" value="house" selected={type} onInputChange={this.handleChange} />
+            <InputCheck
+              labelText="Apartamento"
+              type="radio"
+              name="type"
+              value="apartment"
+              selected={type}
+              onInputChange={this.handleChange}
+            />
+          </fieldset>
+          <fieldset className="second-section">
+            <Input
+              labelText="Resumo do currículo"
+              type="text"
+              name="resume"
+              value={resume}
+              maxLength="1000"
+              isTextarea
+              onInputChange={this.handleChange}
+            />
+            <Input
+              labelText="Cargo"
+              type="text"
+              name="position"
+              value={position}
+              maxLength="40"
+              isTextarea
+              onInputChange={this.handleChange}
+              onMouseEnter={this.handleMouseEnter}
+            />
+            <Input
+              labelText="Descrição do cargo"
+              type="text"
+              name="description"
+              value={description}
+              maxLength="500"
+              onInputChange={this.handleChange}
+            />
           </fieldset>
         </form>
       </div>
--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/my-form-2.0/src/components/Input.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/my-form-2.0/src/components/Input.js
@@ -3,25 +3,44 @@ import PropTypes from 'prop-types';
 
 function Input(props) {
   const {
-    labelText, type, name, value, maxLength, onInputChange, onInputBlur,
+    labelText, type, name, value, maxLength, isTextarea, onInputChange, onInputBlur, onMouseEnter,
   } = props;
   return (
     <div className="input-field">
       <label htmlFor={name}>{labelText}</label>
-      <input
-        type={type}
-        name={name}
-        value={value}
-        maxLength={maxLength}
-        onChange={onInputChange}
-        onBlur={onInputBlur}
-      />
+      {isTextarea
+        ? (
+          <textarea
+            type={type}
+            name={name}
+            id={name}
+            value={value}
+            maxLength={maxLength}
+            onChange={onInputChange}
+            onMouseEnter={onMouseEnter}
+            required
+          />
+        )
+        : (
+          <input
+            type={type}
+            name={name}
+            id={name}
+            value={value}
+            maxLength={maxLength}
+            onChange={onInputChange}
+            onBlur={onInputBlur}
+            required
+          />
+        )}
     </div>
   );
 }
 
 Input.defaultProps = {
+  isTextarea: false,
   onInputBlur: () => {},
+  onMouseEnter: () => {},
 };
 
 Input.propTypes = {
@@ -30,8 +49,10 @@ Input.propTypes = {
   name: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   maxLength: PropTypes.string.isRequired,
+  isTextarea: PropTypes.bool,
   onInputChange: PropTypes.func.isRequired,
   onInputBlur: PropTypes.func,
+  onMouseEnter: PropTypes.func,
 };
 
 export default Input;
