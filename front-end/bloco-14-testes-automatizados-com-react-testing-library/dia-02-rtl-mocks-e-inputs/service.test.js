ae981069042cb5f42eb00596f7c1f11b96ddc37f
Juliano Boese <juliano.boese@gmail.com>
2022-02-22 14:10:01 -0300

    Exercício 14.2 - Parte 1 - Exercício 4

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/service.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/service.test.js
@@ -30,7 +30,7 @@ test('Testa a função generateRandomNumber com mockImplementation e mockReset',
   expect(service.generateRandomNumber).toHaveBeenCalled();
   expect(service.generateRandomNumber).toHaveBeenCalledWith(3, 4, 5);
 
-  service.generateRandomNumber.mockRestore();
+  service.generateRandomNumber.mockReset();
   expect(service.generateRandomNumber).not.toHaveBeenCalled();
 
   service.generateRandomNumber = jest.fn().mockImplementation((a) => 2 * a);
@@ -38,4 +38,33 @@ test('Testa a função generateRandomNumber com mockImplementation e mockReset',
   expect(service.generateRandomNumber(8)).toBe(16);
   expect(service.generateRandomNumber).toHaveBeenCalledTimes(1);
   expect(service.generateRandomNumber).toHaveBeenCalledWith(8);
+});
+
+test('Testa a função convertToUpperCase com jest.spyOn', () => {
+  const convertToLowerCase = jest.spyOn(service, 'convertToUpperCase')
+    .mockImplementation((string) => string.toLowerCase());
+
+  expect(convertToLowerCase('JuLiAnO')).toBe('juliano');
+  expect(convertToLowerCase).toHaveBeenCalled();
+  expect(convertToLowerCase).toHaveBeenCalledWith('JuLiAnO');
+
+  service.convertToUpperCase.mockRestore();
+
+  expect(service.convertToUpperCase('JuLiAnO')).toBe('JULIANO');
+});
+
+test('Testa as funções getFirstLetter e concatStrings com mockImplementation', () => {
+  service.getFirstLetter = jest.fn()
+    .mockImplementation((string) => string.charAt(string.length - 1));
+
+  expect(service.getFirstLetter('Juliano')).toBe('o');
+  expect(service.getFirstLetter).toHaveBeenCalled();
+  expect(service.getFirstLetter).toHaveBeenCalledWith('Juliano');
+
+  service.concatStrings = jest.fn()
+    .mockImplementation((string1, string2, string3) => string1 + string2 + string3);
+
+  expect(service.concatStrings('Juliano', ' é', ' legal')).toBe('Juliano é legal');
+  expect(service.concatStrings).toHaveBeenCalled();
+  expect(service.concatStrings).toHaveBeenCalledWith('Juliano', ' é', ' legal');
 });
