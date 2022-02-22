154a7a9e337972f6a0993bed8571922a72a84c53
Juliano Boese <juliano.boese@gmail.com>
2022-02-22 15:47:26 -0300

    Exercício 14.2 - Parte 1 - Exercício 5

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/service.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/service.js
@@ -6,9 +6,16 @@ const getFirstLetter = (string) => string.charAt(0);
 
 const concatStrings = (string1, string2) => string1 + string2;
 
+const fetchDogImage = async () => {
+  const apiResponse = await fetch('https://dog.ceo/api/breeds/image/random');
+  const responseJson = await apiResponse.json();
+  return responseJson;
+};
+
 module.exports = {
   generateRandomNumber,
   convertToUpperCase,
   getFirstLetter,
   concatStrings,
+  fetchDogImage,
 };
