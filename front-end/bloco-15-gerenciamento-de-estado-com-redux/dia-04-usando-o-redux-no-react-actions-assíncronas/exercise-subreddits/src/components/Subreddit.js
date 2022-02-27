e2d5264ee19c5477761dae1dfd9764a4feb5d836
Juliano Boese <juliano.boese@gmail.com>
2022-02-27 19:20:46 -0300

    Adiciona loading, refresh e timestamp

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-04-usando-o-redux-no-react-actions-assíncronas/exercise-subreddits/src/components/Subreddit.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-04-usando-o-redux-no-react-actions-assíncronas/exercise-subreddits/src/components/Subreddit.js
@@ -5,24 +5,37 @@ import searchSubreddit from '../redux/actions';
 class Subreddit extends Component {
 
   render() {
-    const {selectSubreddit, selectedSubreddit} = this.props;
+    const {loading, selectedSubreddit, timestamp, selectSubreddit} = this.props;
+    const selectedSubredditName = selectedSubreddit && selectedSubreddit[0].data.subreddit;
     return (
       <div>
-        <select onChange={({ target }) => selectSubreddit(target.value)}>
+        <select defaultValue={'Selecione'} onChange={({ target }) => selectSubreddit(target.value)}>
+          <option disabled>Selecione</option>
           <option>frontend</option>
           <option>reactjs</option>
         </select>
-        <h2>Selected: {selectedSubreddit[0].data.subreddit}</h2>
-        <ul>
-          {selectedSubreddit.map((post) => <li>{post.data.title}</li>)}
-        </ul>
+        {loading &&
+          <p>Loading...</p>
+        }
+        {selectedSubreddit && !loading &&
+          <section>
+            <button onClick={() => selectSubreddit(selectedSubredditName)}>Refresh</button>
+            <h2>Selected: {selectedSubreddit[0].data.subreddit}</h2>
+            <p>Last updated at {timestamp}</p>
+            <ul>
+              {selectedSubreddit.map((post) => <li key={post.data.id}>{post.data.title}</li>)}
+            </ul>
+          </section>
+        }        
       </div>
     )
   }
 }
 
 const mapStateToProps = (state) => ({
+  loading: state.loading,
   selectedSubreddit: state.subreddit,
+  timestamp: state.timestamp,
 })
 
 const mapDispatchToProps = (dispatch) => ({
--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-04-usando-o-redux-no-react-actions-assíncronas/exercise-subreddits/src/redux/reducers/index.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-04-usando-o-redux-no-react-actions-assíncronas/exercise-subreddits/src/redux/reducers/index.js
@@ -4,14 +4,23 @@ export const SEARCH_SUBREDDIT = 'SEARCH_SUBREDDIT'
 const INITIAL_STATE = {
   loading: false,
   subreddit: '',
+  timestamp: '',
 };
 
 const reducer = (state = INITIAL_STATE, { type, payload }) => {
   switch (type) {
   case LOADING:
-    return { ...state, loading: true }
+    return {
+      ...state,
+      loading: true
+    }
   case SEARCH_SUBREDDIT:
-    return { ...state, subreddit: payload, loading: false }
+    return {
+      ...state,
+      subreddit: payload,
+      loading: false,
+      timestamp: new Date().toLocaleString()
+    }
   default:
     return state
   }
