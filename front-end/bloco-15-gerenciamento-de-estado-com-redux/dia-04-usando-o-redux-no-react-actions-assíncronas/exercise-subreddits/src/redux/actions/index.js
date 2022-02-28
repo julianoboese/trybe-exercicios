3ef0e44eb3e9090abeed7cc79da1da45333fc17c
Juliano Boese <juliano.boese@gmail.com>
2022-02-27 23:09:34 -0300

    Adiciona redux-logger e tratamento de erro

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-04-usando-o-redux-no-react-actions-assíncronas/exercise-subreddits/src/redux/actions/index.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-04-usando-o-redux-no-react-actions-assíncronas/exercise-subreddits/src/redux/actions/index.js
@@ -1,10 +1,14 @@
 import fetchSubreddit from "../../services/fetchSubreddit";
-import { LOADING, SEARCH_SUBREDDIT } from "../reducers";
+import { LOADING, SEARCH_SUBREDDIT_SUCCESS, SEARCH_SUBREDDIT_ERROR } from "../reducers";
 
 export default function searchSubreddit(subreddit) {
   return async (dispatch) => {
     dispatch({type: LOADING});
-    const payload = await fetchSubreddit(subreddit);
-    dispatch({type: SEARCH_SUBREDDIT, payload})
+    try {
+      const payload = await fetchSubreddit(subreddit);
+      dispatch({type: SEARCH_SUBREDDIT_SUCCESS, payload})
+    } catch(error) {
+      dispatch({type: SEARCH_SUBREDDIT_ERROR, payload: error})
+    }
   }
 }
--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-04-usando-o-redux-no-react-actions-assíncronas/exercise-subreddits/src/redux/reducers/index.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-04-usando-o-redux-no-react-actions-assíncronas/exercise-subreddits/src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 export const LOADING = 'LOADING'
-export const SEARCH_SUBREDDIT = 'SEARCH_SUBREDDIT'
+export const SEARCH_SUBREDDIT_SUCCESS = 'SEARCH_SUBREDDIT_SUCCESS'
+export const SEARCH_SUBREDDIT_ERROR = 'SEARCH_SUBREDDIT_ERROR'
 
 const INITIAL_STATE = {
   loading: false,
@@ -14,12 +15,17 @@ const reducer = (state = INITIAL_STATE, { type, payload }) => {
       ...state,
       loading: true
     }
-  case SEARCH_SUBREDDIT:
+  case SEARCH_SUBREDDIT_SUCCESS:
     return {
       ...state,
       subreddit: payload,
       loading: false,
       timestamp: new Date().toLocaleString()
+    }
+  case SEARCH_SUBREDDIT_ERROR:
+    return {
+      ...state,
+      error: payload,
     }
   default:
     return state
