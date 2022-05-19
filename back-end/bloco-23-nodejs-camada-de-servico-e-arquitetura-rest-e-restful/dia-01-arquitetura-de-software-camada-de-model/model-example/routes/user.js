f023cecf5243d3d6c2b0b502d46879dd20f605b4
Juliano Boese <juliano.boese@gmail.com>
2022-05-19 20:44:07 -0300

    refactor(b23d1): :art: extract validation to middlewares folder

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-01-arquitetura-de-software-camada-de-model/model-example/routes/user.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-01-arquitetura-de-software-camada-de-model/model-example/routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { validation } = require('../middlewares');
 const UserModel = require('../models/User');
 
 const routes = express.Router();
@@ -24,19 +25,10 @@ routes.get('/:id', async (req, res) => {
   return res.status(200).json(user);
 });
 
-routes.post('/', async (req, res) => {
+routes.post('/', validation, async (req, res) => {
   const {
     firstName, lastName, email, password,
   } = req.body;
-
-  if (!firstName || !lastName || !email || !password) {
-    return res.status(400).json({
-      error: true,
-      message: 'Insira todos os dados.',
-    });
-  }
-
-  if (password.length < 6) return res.status(400).json('O campo "password" deve ter pelo menos 6 caracteres');
 
   const newUser = await UserModel.postUser(firstName, lastName, email, password);
 
