c693699eac810f6dd4d73275e31500f70d039545
Juliano Boese <juliano.boese@gmail.com>
2022-05-22 16:04:15 -0300

    feat(b23d2): :sparkles: add cep validation on cepController
    WIP - exercise 2

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/controllers/cepController.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/controllers/cepController.js
@@ -1,4 +1,20 @@
-const getCep = () => ({});
+const Joi = require('joi');
+
+const getCep = (req, res) => {
+  const { cep } = req.params;
+
+  const { error } = Joi.string()
+    .pattern(/^\d{5}-?\d{3}$/, 'cep')
+    .validate(cep);
+
+  if (error) {
+    return res
+      .status(400)
+      .json({ error: { code: 'invalidData', message: 'CEP inválido' } });
+  }
+
+  return res.status(200).json({ message: 'CEP válido' });
+};
 
 module.exports = {
   getCep,
