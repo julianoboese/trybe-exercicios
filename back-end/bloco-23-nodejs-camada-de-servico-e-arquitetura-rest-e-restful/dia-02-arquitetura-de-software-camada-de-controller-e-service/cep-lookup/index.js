99abbdaa943034a7566110b3b38a96fd108f9146
Juliano Boese <juliano.boese@gmail.com>
2022-05-23 14:58:22 -0300

    feat(b23d2): :goal_net: create error middleware
    WIP - exercise 2

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const { error } = require('./middlewares');
 const { cepRoutes } = require('./routes');
 
 const app = express();
@@ -9,6 +10,8 @@ app.use(express.json());
 app.get('/ping', (_req, res) => res.status(200).json({ message: 'pong!' }));
 
 app.use(cepRoutes);
+
+app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}!`));
---
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/middlewares/error.js
@@ -0,0 +1,11 @@
+const errorCodes = {
+  invalidData: 400,
+  notFound: 404,
+};
+
+const errorMiddleware = (error, _req, res, _next) => {
+  const { code, message } = error;
+  res.status(errorCodes[code] || 500).json({ error: message });
+};
+
+module.exports = errorMiddleware;
