2ba822b8a9b19e535ac63ce1338be71228cd068a
Juliano Boese <juliano.boese@gmail.com>
2022-05-24 01:03:32 -0300

    feat(b23d2): :sparkles: request to external API in GET /cep/:cep if cep not in db
    WIP - bonus exercise 1

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/models/cepModel.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/models/cepModel.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 const connection = require('../db');
 
 const getCep = async (cep) => {
@@ -5,6 +7,12 @@ const getCep = async (cep) => {
     'SELECT * FROM cep_lookup.ceps WHERE cep = ?',
     [cep],
   );
+
+  return cepData;
+};
+
+const getCepFromApi = async (cep) => {
+  const cepData = await (await fetch(`https://viacep.com.br/ws/${cep}/json/`)).json();
 
   return cepData;
 };
@@ -20,5 +28,6 @@ const postCep = async (newCepData) => {
 
 module.exports = {
   getCep,
+  getCepFromApi,
   postCep,
 };
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/services/cepService.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/services/cepService.js
@@ -1,12 +1,19 @@
 const cepModel = require('../models/cepModel');
 
-const getCep = async (cep) => {
-  const standardCep = cep.includes('-') ? cep : `${cep.slice(0, 5)}-${cep.slice(5)}`;
+const getCep = async (reqCep) => {
+  const standardCep = reqCep.includes('-') ? reqCep : `${reqCep.slice(0, 5)}-${reqCep.slice(5)}`;
 
   const cepData = await cepModel.getCep(standardCep);
 
   if (cepData.length === 0) {
-    return { error: { code: 'notFound', message: 'CEP não encontrado' } };
+    const apiCep = await cepModel.getCepFromApi(reqCep);
+
+    if (apiCep.erro) {
+      return { error: { code: 'notFound', message: 'CEP não encontrado' } };
+    }
+
+    const { cep, logradouro, bairro, localidade, uf } = apiCep;
+    return { cep, logradouro, bairro, localidade, uf };
   }
 
   return cepData;
