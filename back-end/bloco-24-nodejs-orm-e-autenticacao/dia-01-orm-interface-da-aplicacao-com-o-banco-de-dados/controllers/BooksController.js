820bebb22bc5254230692490dde72352855777ca
Juliano Boese <juliano.boese@gmail.com>
2022-06-10 00:09:47 -0300

    feat(b24d1): :sparkles: create getById method in BooksController
    Exercise 7 - COMPLETE

--- back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/controllers/BooksController.js
+++ back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/controllers/BooksController.js
@@ -6,6 +6,17 @@ const getAll = async (_req, res) => {
   res.status(200).json(allBooks);
 };
 
+const getById = async (req, res) => {
+  const { id } = req.params;
+
+  const book = await BooksService.getById(id);
+
+  if (!book) return res.status(404).json({ message: 'Book not found' });
+
+  return res.status(200).json(book);
+};
+
 module.exports = {
   getAll,
+  getById,
 };
