c12ed9908589109bba78495773dd915729addacb
Juliano Boese <juliano.boese@gmail.com>
2022-06-10 00:43:49 -0300

    feat(b24d1): :sparkles: create the PUT /books/:id route
    Exercise 14 - COMPLETE

--- back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
+++ back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.get('/books', BooksController.getAll);
 app.get('/books/:id', BooksController.getById);
 app.post('/books', BooksController.create);
+app.put('/books/:id', BooksController.update);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
