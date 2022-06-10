e1e109c4ddeff33239d845e29aa8478d64784515
Juliano Boese <juliano.boese@gmail.com>
2022-06-10 00:51:52 -0300

    feat(b24d1): :sparkles: create the DELETE /books/:id route
    Exercise 17 - COMPLETE

--- back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
+++ back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
@@ -9,6 +9,7 @@ app.get('/books', BooksController.getAll);
 app.get('/books/:id', BooksController.getById);
 app.post('/books', BooksController.create);
 app.put('/books/:id', BooksController.update);
+app.delete('/books/:id', BooksController.remove);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
