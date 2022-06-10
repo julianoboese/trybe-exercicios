c803f56add08ba2b6f1d81fdbe6bbf0d5ed9ce5b
Juliano Boese <juliano.boese@gmail.com>
2022-06-09 23:55:45 -0300

    feat: :sparkles: create the GET /books route
    Exercise 5 - COMPLETE

--- back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
+++ back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const BooksController = require('./controllers/BooksController');
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/books', BooksController.getAll);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
