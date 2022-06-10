87edfbd810b1d588f2763901326a0e7afafd1a93
Juliano Boese <juliano.boese@gmail.com>
2022-06-10 00:23:54 -0300

    feat(b24d1): :sparkles: create the POST /books route
    Exercise 11 - COMPLETE

--- back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
+++ back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
@@ -7,6 +7,7 @@ app.use(express.json());
 
 app.get('/books', BooksController.getAll);
 app.get('/books/:id', BooksController.getById);
+app.post('/books', BooksController.create);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
