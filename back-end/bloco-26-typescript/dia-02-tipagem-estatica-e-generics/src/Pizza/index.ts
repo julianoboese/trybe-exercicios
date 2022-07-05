f2e8c0246801defcc1039a30332c5cb210ba0877
Juliano Boese <juliano.boese@gmail.com>
2022-07-05 15:52:19 -0300

    feat(pizza): :sparkles: create three types of pizza
    ex3 - done

---
+++ back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/src/Pizza/index.ts
@@ -0,0 +1,20 @@
+import Pizza from './Pizza';
+
+const calabresa: Pizza = {
+  flavor: 'Calabresa',
+  slices: 8,
+};
+
+const marguerita: Pizza = {
+  flavor: 'Marguerita',
+  slices: 6,
+};
+
+const nutella: Pizza = {
+  flavor: 'Nutella',
+  slices: 4,
+};
+
+console.log(calabresa);
+console.log(marguerita);
+console.log(nutella);
