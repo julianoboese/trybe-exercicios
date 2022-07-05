17a89310b0998882bb4306c8da92d75b2934557e
Juliano Boese <juliano.boese@gmail.com>
2022-07-05 16:02:56 -0300

    feat(pizza): :sparkles: create pizzas from all types
    ex4 - done

--- back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/src/Pizza/index.ts
+++ back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/src/Pizza/index.ts
@@ -1,20 +1,40 @@
-import Pizza from './Pizza';
+import CommonPizza from './CommonPizza';
+import SugarPizza from './SugarPizza';
+import VegetarianPizza from './VegetarianPizza';
 
-const calabresa: Pizza = {
+const calabresa: CommonPizza = {
   flavor: 'Calabresa',
   slices: 8,
 };
 
-const marguerita: Pizza = {
+const frango: CommonPizza = {
+  flavor: 'Frango',
+  slices: 6,
+};
+
+const pepperoni: CommonPizza = {
+  flavor: 'Pepperoni',
+  slices: 4,
+};
+
+const marguerita: VegetarianPizza = {
   flavor: 'Marguerita',
   slices: 6,
 };
 
-const nutella: Pizza = {
+const palmito: VegetarianPizza = {
+  flavor: 'Palmito',
+  slices: 8,
+};
+
+const nutella: SugarPizza = {
   flavor: 'Nutella',
   slices: 4,
 };
 
 console.log(calabresa);
+console.log(frango);
+console.log(pepperoni);
 console.log(marguerita);
+console.log(palmito);
 console.log(nutella);
